b442bf7c1537a1bdacd65ce2493004e12155485c
Enrique Sanchez <esanchez.devel@gmail.com>
2023-11-01 21:28:49 +0100

    refactor: rename main package

--- src/main/java/com/esanchez/devel/notifications/control/NotificationsControlApplication.java
+++ src/main/java/com/esanchez/devel/notifications/control/NotificationsControlApplication.java
@@ -1,4 +1,4 @@
-package com.esanchez.devel.notificationscontrol;
+package com.esanchez.devel.notifications.control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
