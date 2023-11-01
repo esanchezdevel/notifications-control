bffad807741f29b2be2ca77b8469becc45b61e2e
Enrique Sanchez <esanchez.devel@gmail.com>
2023-11-01 12:54:09 +0100

    feat: first commit

---
+++ src/main/java/com/esanchez/devel/notificationscontrol/NotificationsControlApplication.java
@@ -0,0 +1,13 @@
+package com.esanchez.devel.notificationscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esanchez/devel/notificationscontrol/NotificationsControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esanchez.devel.notificationscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
