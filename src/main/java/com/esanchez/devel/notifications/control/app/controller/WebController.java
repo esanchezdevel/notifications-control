e46ab84db79a505cd467152bc66347028af0fe37
Enrique Sanchez <esanchez.devel@gmail.com>
2023-11-02 21:35:56 +0100

    feat: get index title and version from constants class

--- src/main/java/com/esanchez/devel/notifications/control/app/controller/WebController.java
+++ src/main/java/com/esanchez/devel/notifications/control/app/controller/WebController.java
@@ -4,13 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.esanchez.devel.notifications.control.infrastructure.Constants;
+
 @Controller
 public class WebController {
 
 	@GetMapping("/")
 	public String index(Model model) {
 		
-		model.addAttribute("title", "Notifications Controller");
+		model.addAttribute("title", Constants.TITLE);
+		model.addAttribute("version", Constants.VERSION);
 		
 		return "index";
 	}
