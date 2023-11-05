ec9eb68fb2d256bc2e4d3641f7240117c3babeea
Enrique Sanchez <esanchez.devel@gmail.com>
2023-11-05 11:50:15 +0100

    feat: create an API response DTO to add response data

--- src/main/java/com/esanchez/devel/notifications/control/app/controller/ApiController.java
+++ src/main/java/com/esanchez/devel/notifications/control/app/controller/ApiController.java
@@ -1,11 +1,13 @@
 package com.esanchez.devel.notifications.control.app.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.esanchez.devel.notifications.control.app.dto.ApiResponseDTO;
 import com.esanchez.devel.notifications.control.app.dto.SystemRegisterDTO;
 
 @RestController
@@ -13,10 +15,13 @@ import com.esanchez.devel.notifications.control.app.dto.SystemRegisterDTO;
 public class ApiController {
 
 	@PostMapping("/system/register")
-	public ResponseEntity<?> systemRegister(@RequestBody SystemRegisterDTO request) {
+	public ResponseEntity<ApiResponseDTO> systemRegister(@RequestBody SystemRegisterDTO request) {
 		
 		System.out.println("Request received: " + request);
 		
-		return ResponseEntity.ok("{\"result\": \"OK\"}");
+		ApiResponseDTO response = new ApiResponseDTO();
+		response.setCode(HttpStatus.OK.value());
+		response.setMessage("OK");
+		return ResponseEntity.ok(response);
 	}
 }
